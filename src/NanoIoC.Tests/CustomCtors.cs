4bf78d03fbd3fb9e82541986b5e374addc4660b3
Andrew Bullock <trullock@googlemail.com>
2011-10-12 14:31:16 +0100

    custom ctors

---
+++ src/NanoIoC.Tests/CustomCtors.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+    [TestFixture]
+    public class CustomCtors
+    {
+        [Test]
+        public void TheContainShouldBeInsideItself()
+        {
+            var container = new Container();
+
+			container.Register<TestInterface>(c => new TestClass());
+
+			var resolved = container.Resolve<TestInterface>();
+
+            Assert.AreSame(typeof(TestClass), resolved.GetType());
+        }
+
+        public class TestClass : TestInterface
+        {
+            
+        }
+		public interface TestInterface
+		{
+			
+		}
+    }
+}
