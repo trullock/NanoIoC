8deb5497cb25ea65559b75bcc1d4bc9fbcbd0491
Andrew Bullock <trullock@googlemail.com>
2011-11-07 14:18:28 +0000

    added ejecting by lifecycle

---
+++ src/NanoIoC.Tests/MultiThreadingWithClearing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+    [TestFixture]
+    public class MultiThreadingWithClearing
+    {
+    	Container container;
+    	Exception ex;
+
+    	[Test]
+        public void ShouldConstruct()
+        {
+            this.container = new Container();
+			this.container.Register<TestInterface, TestClass>(Lifecycle.HttpContextOrThreadLocal);
+
+			for (var i = 0; i < 10; i++)
+				ThreadPool.QueueUserWorkItem(this.Test);
+
+			Thread.Sleep(1000);
+
+           Assert.IsNull(ex);
+        }
+
+		void Test(object state)
+		{
+			try
+			{
+				container.Resolve<TestInterface>();
+				container.RemoveAllInstancesWithLifecycle(Lifecycle.HttpContextOrThreadLocal);
+			}
+			catch(Exception e)
+			{
+				ex = e;
+			}
+		}
+
+		public class TestClass : TestInterface
+        {
+            
+        }
+
+		public interface TestInterface
+		{
+			
+		}
+    }
+}
