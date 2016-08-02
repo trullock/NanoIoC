3dae782d6a0ae8f7b27c02926b54f21456003ddd
Chris Nevett <chris.nevett@myunidays.com>
2016-08-02 16:43:24 +0100

    add tests

---
+++ src/NanoIoC.Tests/ExecutionContextScopeAcrossThreads.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+    [TestFixture]
+    public class ExecutionContextScopeAcrossThreads
+	{
+        [Test]
+        public void ShouldConstruct()
+        {
+            var container = new Container();
+			container.Register<TestInterface, TestClass>(Lifecycle.ExecutionContextLocal);
+
+
+	        TestInterface instance = null;
+
+	        Task.Run(() => instance = container.Resolve<TestInterface>()).Wait();
+
+			Assert.IsNotNull(instance);
+			Assert.IsInstanceOf<TestClass>(instance);
+        }
+
+		[Test]
+		public void ShouldAlwaysBeTheSameInstance()
+		{
+			var container = new Container();
+			container.Register<TestInterface, TestClass>();
+
+			TestInterface instance = null;
+			TestInterface instance2 = null;
+
+			Task.Run(() =>
+			{
+				instance = container.Resolve<TestInterface>();
+				instance2 = container.Resolve<TestInterface>();
+			}).Wait();
+
+			Assert.IsNotNull(instance);
+			Assert.AreSame(instance, instance2);
+		}
+
+    	public class TestClass : TestInterface
+        {
+            
+        }
+
+		public interface TestInterface
+		{
+			
+		}
+    }
+}
---
+++ src/NanoIoC.Tests/RegisteredConcreteTypesWithExecutionContextScope.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+    [TestFixture]
+    public class RegisteredConcreteTypesWithExecutionContextScope
+    {
+        [Test]
+        public void ShouldConstruct()
+        {
+            var container = new Container();
+			container.Register<TestInterface, TestClass>(Lifecycle.ExecutionContextLocal);
+
+			var instance = container.Resolve<TestInterface>();
+            Assert.IsInstanceOf<TestClass>(instance);
+        }
+
+		[Test]
+		public void ShouldAlwaysBeTheSameInstance()
+		{
+			var container = new Container();
+			container.Register<TestInterface, TestClass>();
+
+			var instance = container.Resolve<TestInterface>();
+			var instance2 = container.Resolve<TestInterface>();
+			Assert.AreSame(instance, instance2);
+		}
+
+    	public class TestClass : TestInterface
+        {
+            
+        }
+
+		public interface TestInterface
+		{
+			
+		}
+    }
+}
