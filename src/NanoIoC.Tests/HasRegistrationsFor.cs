0ccb75c3ab5033d1d78c3a99d8b3d9684a57f35f
Chris Nevett <chris.nevett@myunidays.com>
2017-04-20 17:29:21 +0100

    add tests for HasRegistrationsFor

---
+++ src/NanoIoC.Tests/HasRegistrationsFor.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+	[TestFixture]
+	public class HasRegistrationsFor
+	{
+		[Test]
+		public void RegisteredTransient()
+		{
+			RegisteredLifecycle(Lifecycle.Singleton);
+		}
+
+		[Test]
+		public void RegisteredSingleton()
+		{
+			RegisteredLifecycle(Lifecycle.Singleton);
+		}
+
+		[Test]
+		public void RegisteredThreadLocal()
+		{
+			RegisteredLifecycle(Lifecycle.HttpContextOrThreadLocal);
+		}
+
+		[Test]
+		public void RegisteredExecutionContextLocal()
+		{
+			RegisteredLifecycle(Lifecycle.HttpContextOrExecutionContextLocal);
+		}
+
+		[Test]
+		public void InjectedSingleton()
+		{
+			InjectedLifecycle(Lifecycle.Singleton);
+		}
+
+		[Test]
+		public void InjectedThreadLocal()
+		{
+			InjectedLifecycle(Lifecycle.HttpContextOrThreadLocal);
+		}
+
+		[Test]
+		public void InjectedExecutionContextLocal()
+		{
+			InjectedLifecycle(Lifecycle.HttpContextOrExecutionContextLocal);
+		}
+
+		static void InjectedLifecycle(Lifecycle lifecycle)
+		{
+			var container = new Container();
+
+			Assert.IsFalse(container.HasRegistrationsFor(typeof(TestClass)));
+
+			container.Inject(new TestClass(), lifecycle);
+
+			Assert.IsTrue(container.HasRegistrationsFor(typeof(TestClass)));
+		}
+
+		static void RegisteredLifecycle(Lifecycle lifecycle)
+		{
+			var container = new Container();
+
+			Assert.IsFalse(container.HasRegistrationsFor(typeof(ITestClass)));
+
+			container.Register(typeof(ITestClass), typeof(TestClass), lifecycle);
+
+			Assert.IsTrue(container.HasRegistrationsFor(typeof(ITestClass)));
+		}
+
+		public interface ITestClass
+		{
+			
+		}
+
+		public class TestClass : ITestClass
+		{
+
+		}
+	}
+}
