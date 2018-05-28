ac4f6d91ef0c08b7a14b2c6079c4fde1ebef0cc8
Tim Barker <tim.barker@myunidays.com>
2018-05-28 09:08:05 +0100

    moves HasRegistrationsFor onto IResloverContainer

--- src/NanoIoC/IResolverContainer.cs
+++ src/NanoIoC/IResolverContainer.cs
@@ -13,5 +13,12 @@ namespace NanoIoC
 		object Resolve(Type type);
 		IEnumerable ResolveAll(Type abstractType);
 		object Resolve(Type type, params object[] dependencies);
+
+		/// <summary>
+		/// Determines if there is a registration for the given type
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		bool HasRegistrationsFor(Type type);
 	}
 }
