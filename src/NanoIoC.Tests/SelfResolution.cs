e630280d011e0074a2fe5be0c4d604749af148c8
Andrew Bullock <trullock@gmail.com>
2018-08-10 14:22:18 +0100

    added self-resolution test (failing)

---
+++ src/NanoIoC.Tests/SelfResolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NanoIoC.Tests
+{
+	[TestFixture]
+	public class SelfResolution
+	{
+		[Test]
+		public void ContainerShouldResolveItselfAsIContianer()
+		{
+			var container = new Container();
+
+			Assert.AreSame(container, container.Resolve<IContainer>());
+		}
+
+		[Test]
+		public void ContainerShouldResolveItselfAsIResolverContianer()
+		{
+			var container = new Container();
+
+			Assert.AreSame(container, container.Resolve<IResolverContainer>());
+		}
+	}
+}
