f49ead5c12982a7367c584d9c5024fa7f23f0117
Oliver Dunthorne <oliver.dunthorne@myunidays.com>
2019-02-18 09:21:39 +0000

    removed reference to Microsoft.AspNetCore.Http as not required

--- src/NanoIoC/Container.cs
+++ src/NanoIoC/Container.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NanoIoC
