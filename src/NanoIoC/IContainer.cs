3bd0e4a98e50528baff653c2e797d6b6bc97bb9b
Andrew Bullock <trullock@gmail.com>
2019-04-16 20:38:02 +0100

    more interface refactorings

--- src/NanoIoC/IContainer.cs
+++ src/NanoIoC/IContainer.cs
@@ -7,13 +7,6 @@ namespace NanoIoC
     public interface IContainer : IResolverContainer
     {
 		/// <summary>
-		/// Gets the registered concrete types for the requested type
-		/// </summary>
-		/// <param name="type"></param>
-		/// <returns></returns>
-        IEnumerable<Registration> GetRegistrationsFor(Type type);
-		
-		/// <summary>
 		/// Registers the given concrete type against the given abstract type with the given serviceLifetime
 		/// </summary>
 		/// <param name="abstractType"></param>
@@ -21,18 +14,69 @@ namespace NanoIoC
 		/// <param name="lifetime"></param>
         void Register(Type abstractType, Type concreteType, ServiceLifetime lifetime);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="abstractType"></param>
+		/// <param name="ctor"></param>
+		/// <param name="lifetime"></param>
 		void Register(Type abstractType, Func<IResolverContainer, object> ctor, ServiceLifetime lifetime);
 
-	    /// <summary>
-	    /// Injects the given instance as the given type with the given serviceLifetime
-	    /// </summary>
-	    /// <param name="instance"></param>
-	    /// <param name="type"></param>
-	    /// <param name="lifetime">Must not be Transient</param>
-	    /// <param name="injectionBehaviour"></param>
-	    void Inject(object instance, Type type, ServiceLifetime lifetime, InjectionBehaviour injectionBehaviour);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="TAbstract"></typeparam>
+		/// <param name="ctor"></param>
+		/// <param name="lifetime"></param>
+		void Register<TAbstract>(Func<IResolverContainer, TAbstract> ctor, ServiceLifetime lifetime = ServiceLifetime.Singleton);
 
+		/// <summary>
+		/// Registers a concrete type for an abstract type
+		/// </summary>
+		/// <typeparam name="TAbstract">The abstract type you want to resolve later</typeparam>
+		/// <typeparam name="TConcrete">The concrete type implementing the abstract type</typeparam>
+		/// <param name="lifetime"></param>
+		void Register<TAbstract, TConcrete>(ServiceLifetime lifetime = ServiceLifetime.Singleton) where TConcrete : TAbstract;
+
+		/// <summary>
+		/// Registers a concrete type with a serviceLifetime.
+		/// You should only be using this for Singleton or HttpContextOrThreadLocal serviceLifetimes.
+		/// Registering a concrete type as Transient is pointless, as you get this behaviour by default.
+		/// </summary>
+		/// <typeparam name="TConcrete">The concrete type to register</typeparam>
+		/// <param name="lifetime">The serviceLifetime of the instance</param>
+		void Register<TConcrete>(ServiceLifetime lifetime);
+
+
+		/// <summary>
+		/// Injects the given instance as the given type with the given serviceLifetime
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <param name="type"></param>
+		/// <param name="lifetime">Must not be Transient</param>
+		/// <param name="injectionBehaviour"></param>
+		void Inject(object instance, Type type, ServiceLifetime lifetime, InjectionBehaviour injectionBehaviour);
+
+		/// <summary>
+		/// Injects an instance 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="instance"></param>
+		/// <param name="lifetime"></param>
+		/// <param name="injectionBehaviour"></param>
+		void Inject<T>(T instance, ServiceLifetime lifetime = ServiceLifetime.Singleton, InjectionBehaviour injectionBehaviour = InjectionBehaviour.Default);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="type"></param>
 		void RemoveAllRegistrationsAndInstancesOf(Type type);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		void RemoveAllRegistrationsAndInstancesOf<T>();
 
 		/// <summary>
 		/// Removes all instances with the given serviceLifetime
@@ -40,68 +84,23 @@ namespace NanoIoC
 		/// <param name="serviceLifetime"></param>
 		void RemoveAllInstancesWithServiceLifetime(ServiceLifetime serviceLifetime);
 
-		// Empty the container of everything! (apart from itself)
-    	void Reset();
+		/// <summary>
+		/// Empty the container of everything! (apart from itself)
+		/// </summary>
+		void Reset();
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="serviceLifetime"></param>
 	    void RemoveInstancesOf(Type type, ServiceLifetime serviceLifetime);
-	    void Register<TAbstract>(Func<IResolverContainer, TAbstract> ctor, ServiceLifetime lifetime = ServiceLifetime.Singleton);
 
-	    /// <summary>
-	    /// Registers a concrete type for an abstract type
-	    /// </summary>
-	    /// <typeparam name="TAbstract">The abstract type you want to resolve later</typeparam>
-	    /// <typeparam name="TConcrete">The concrete type implementing the abstract type</typeparam>
-	    /// <param name="lifetime"></param>
-	    void Register<TAbstract, TConcrete>(ServiceLifetime lifetime = ServiceLifetime.Singleton) where TConcrete : TAbstract;
-
-	    /// <summary>
-	    /// Registers a concrete type with a serviceLifetime.
-	    /// You should only be using this for Singleton or HttpContextOrThreadLocal serviceLifetimes.
-	    /// Registering a concrete type as Transient is pointless, as you get this behaviour by default.
-	    /// </summary>
-	    /// <typeparam name="TConcrete">The concrete type to register</typeparam>
-	    /// <param name="lifetime">The serviceLifetime of the instance</param>
-	    void Register<TConcrete>(ServiceLifetime lifetime);
-
-	    /// <summary>
-	    /// Injects an instance 
-	    /// </summary>
-	    /// <typeparam name="T"></typeparam>
-	    /// <param name="instance"></param>
-	    /// <param name="lifetime"></param>
-	    /// <param name="injectionBehaviour"></param>
-	    void Inject<T>(T instance, ServiceLifetime lifetime = ServiceLifetime.Singleton, InjectionBehaviour injectionBehaviour = InjectionBehaviour.Default);
-
-	    /// <summary>
-	    /// Resolves all registered instances of T
-	    /// </summary>
-	    /// <typeparam name="T"></typeparam>
-	    /// <returns></returns>
-	    IEnumerable<T> ResolveAll<T>();
-
-	    /// <summary>
-	    /// Resolves
-	    /// </summary>
-	    /// <typeparam name="T"></typeparam>
-	    /// <returns></returns>
-	    T Resolve<T>();
-
-	    object Resolve<T>(params object[] dependencies);
-
-	    /// <summary>
-	    /// Returns the dependency graph for the given type
-	    /// </summary>
-	    /// <typeparam name="T"></typeparam>
-	    /// <returns></returns>
-	    GraphNode DependencyGraph<T>();
-
-	    /// <inheritdoc />
-	    void RemoveAllRegistrationsAndInstancesOf<T>();
-
-	    /// <inheritdoc />
-	    void RemoveInstancesOf<T>(ServiceLifetime lifetime);
-
-	    /// <inheritdoc />
-	    bool HasRegistrationFor<T>();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="lifetime"></param>
+		void RemoveInstancesOf<T>(ServiceLifetime lifetime);
     }
 }
--- src/NanoIoC/IResolverContainer.cs
+++ src/NanoIoC/IResolverContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace NanoIoC
 {
@@ -11,8 +12,43 @@ namespace NanoIoC
 		/// <param name="type"></param>
 		/// <returns></returns>
 		object Resolve(Type type);
-		IEnumerable ResolveAll(Type abstractType);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="dependencies"></param>
+		/// <returns></returns>
 		object Resolve(Type type, params object[] dependencies);
+
+		/// <summary>
+		/// Resolves
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		T Resolve<T>();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="dependencies"></param>
+		/// <returns></returns>
+		object Resolve<T>(params object[] dependencies);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="abstractType"></param>
+		/// <returns></returns>
+		IEnumerable ResolveAll(Type abstractType);
+
+		/// <summary>
+		/// Resolves all registered instances of T
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		IEnumerable<T> ResolveAll<T>();
 
 		/// <summary>
 		/// Returns a graph of the dependencies for the requested type
@@ -22,10 +58,31 @@ namespace NanoIoC
 		GraphNode DependencyGraph(Type type);
 
 		/// <summary>
+		/// Returns the dependency graph for the given type
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		GraphNode DependencyGraph<T>();
+
+		/// <summary>
 		/// Determines if there is a registration for the given type
 		/// </summary>
 		/// <param name="type"></param>
 		/// <returns></returns>
 		bool HasRegistrationsFor(Type type);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		bool HasRegistrationFor<T>();
+
+		/// <summary>
+		/// Gets the registered concrete types for the requested type
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		IEnumerable<Registration> GetRegistrationsFor(Type type);
 	}
 }
